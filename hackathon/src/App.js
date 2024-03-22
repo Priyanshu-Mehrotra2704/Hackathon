64eb92dc4a069de9f31eac4b9348ec2c7cb38fb9
beherabibekananda <beherabibekananda778@gmail.com>
2024-03-23 00:34:32 +0530

    login

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (<main>
+  <div>
+        <div className="navbar">
+            <a href='/' className='logo'>Recipe</a>
+            <a className="nav" href="home.html">Home</a>
+            <a  className="nav" href="planvisit.html">Recipe</a>
+            <a  className="nav" href="hotels.html">Add Recipes</a>
+            <a  className="nav" href="loginorsignin.html">My Recipe</a>
+            <a  className="nav" href="contactus.html">Profile</a>
+            <div>
+            <input type="text" id="search" placeholder="    Search......"/>
+            <input class="btn btn-primary" type="submit" value="Submit"/>
+            </div>
+        </div>
+    </div>
+  </main>
   );
 }
 
